ae12826127e307dad7b3face047b47937d2ae543
João Marcos Barbosa Silva <joaobarbosa@sefaz.pi.gov.br>
2022-09-01 13:31:51 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@/assets/css/tailwind.css'
+import '@/assets/css/fonts.css'
+import 'animate.css'
+import router from './router'
 
 createApp(App).mount('#app')
